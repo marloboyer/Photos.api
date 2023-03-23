ffb53ec76b505c1f474927a317bf9c8d6e513fa0
marloboyer <marlo.boyer7@yahoo.com>
2023-03-23 17:31:44 -0500

    created create action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,15 @@
 class PhotosController < ApplicationController
+  def index
+    @photo = Photo.all
+    render :index
+  end
+
+  def create
+    @photo = Photo.create(
+      name: params[:name],
+      width: params[:width],
+      hegith: params[:height],
+    )
+    render :show
+  end
 end
--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/photos.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    pp data
+    assert_equal Photo.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Photo.count", 1 do
+      post "/photos.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
 end
